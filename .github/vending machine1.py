468fcb294cb0401843d2c8cc42f9099f60749053
GITHUB_USERNAME <KEHILLAH_EMAIL>
2026-01-07 13:02:30 -0800

    all done

--- .github/vending machine1.py
+++ .github/vending machine1.py
@@ -1,15 +1,16 @@
-current_state = "locked"
+def snack_machine(current_state):
 
-while current_state != "coin paid":
+    if current_state == "coin paid":
+        return
 
     if current_state == "locked":
         print("no snack for u")
     elif current_state == "paid":
         print("here u go")
     else:
-        print("what do you want from me idk what your asking")
+        print("what do you want from me idk what you're asking")
 
-    current_input = input("You can add a coin (c) or you can ask for a snack (p) or you can take your snack(t)")
+    current_input = input("You can add a coin (c), ask for a snack (p), or take your snack (t): ")
 
     if current_state == "locked":
         if current_input == "c":
@@ -18,7 +19,7 @@ while current_state != "coin paid":
             print("please insert coin")
         elif current_input == "t":
             print("give me money first")
-    
+
     elif current_state == "paid":
         if current_input == "c":
             current_state = "unlocked"
@@ -26,12 +27,19 @@ while current_state != "coin paid":
         elif current_input == "p":
             print("take your snack")
             current_state = "unlocked"
-        
+
     elif current_state == "unlocked":
         if current_input == "t":
             print("here you go")
             current_state = "locked"
-        elif current_input =="c":
+        elif current_input == "c":
             print("coin rejected, take your snack already")
         elif current_input == "p":
-            print("you already have one you ungreatful child")
+            print("you already have one you ungrateful child")
+
+
+    snack_machine(current_state)
+
+
+
+snack_machine("locked")
